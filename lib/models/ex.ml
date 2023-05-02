78ace6c9718cbce5a892137f199e32b724b2f318
TJ DeVries <devries.timothyj@gmail.com>
2023-05-02 00:37:30 -0400

    LET HIM COOK

--- lib/models/ex.ml
+++ lib/models/ex.ml
@@ -2,13 +2,16 @@ module User = struct
   type t =
     { id : int
     ; name : string
-    ; user_id : int
+    ; image_id : int [@foreign images]
     }
   [@@deriving model ~table_name:"users"]
 end
 
 let do_something db =
-  let%lwt _ = User.create ~name:"hello" ~user_id:10 db in
+  let%lwt _ = User.create ~name:"hello" ~image_id:10 db in
+  let%lwt _ = User.create_record { name = "something"; image_id = 5 } db in
+  let%lwt _ = User.read_all () db in
+  let%lwt _ = User.get_by_image_id ~image_id:5 db in
   let%lwt user = User.read ~id:1 db in
   match user with
   | Ok (Some user) -> user.name |> Lwt.return
--- ppx-model/model.ml
+++ ppx-model/model.ml
@@ -21,6 +21,61 @@ let label_to_input label =
 
 let make_str_const s loc = Exp.constant (Pconst_string (s, loc, None))
 
+let mk_select ~selections ~table_name ~id_label ~loc =
+  let selection =
+    Fmt.str
+      {| SELECT %s FROM %s WHERE id = %s |}
+      selections
+      table_name
+      (label_to_input id_label)
+  in
+  make_str_const selection loc
+;;
+
+let mk_select_many ~selections ~table_name ~loc =
+  let select_many = Fmt.str {| SELECT %s FROM %s |} selections table_name in
+  make_str_const select_many loc
+;;
+
+let mk_create ~id_label ~non_id_labels ~loc ~table_name =
+  let create_select =
+    List.map non_id_labels ~f:(fun l -> l.pld_name.txt)
+    |> String.concat ~sep:","
+  in
+  let create_values =
+    List.map non_id_labels ~f:label_to_input |> String.concat ~sep:","
+  in
+  let create =
+    "INSERT INTO "
+    ^ table_name
+    ^ " ("
+    ^ create_select
+    ^ ") VALUES ("
+    ^ create_values
+    ^ ") RETURNING "
+    ^ label_to_output id_label
+  in
+  make_str_const create loc
+;;
+
+let mk_get_by_id ~selections ~table_name ~key ~foreign_name ~foreign_key ~loc =
+  let get_query =
+    Fmt.str
+      {| SELECT %s FROM %s INNER JOIN %s ON %s.%s = %s.%s WHERE %s.%s = %%int{%s} |}
+      selections
+      table_name
+      foreign_name
+      table_name
+      key
+      foreign_name
+      foreign_key
+      table_name
+      key
+      key
+  in
+  make_str_const get_query loc
+;;
+
 let generate_impl ~ctxt (_rec_flag, type_decls) table_name =
   let loc = Expansion_context.Deriver.derived_item_loc ctxt in
   List.map type_decls ~f:(function
@@ -32,66 +87,80 @@ let generate_impl ~ctxt (_rec_flag, type_decls) table_name =
       let non_id_labels =
         List.filter labels ~f:(fun l -> String.(l.pld_name.txt <> "id"))
       in
-      let selection =
+      let selections =
         List.map labels ~f:label_to_output |> String.concat ~sep:","
       in
-      let selection =
-        "SELECT "
-        ^ selection
-        ^ " FROM "
-        ^ table_name
-        ^ " where id = "
-        ^ label_to_input id_label
+      let foreign =
+        List.find labels ~f:(fun l ->
+          List.find l.pld_attributes ~f:(fun a ->
+            String.(a.attr_name.txt = "foreign"))
+          |> Option.is_some)
       in
-      let selection = make_str_const selection loc in
-      let create_select =
-        List.map non_id_labels ~f:(fun l -> l.pld_name.txt)
-        |> String.concat ~sep:","
+      let foreign =
+        Option.map foreign ~f:(fun foreign ->
+          let payload = (List.hd_exn foreign.pld_attributes).attr_payload in
+          let foreign_key = foreign.pld_name.txt in
+          let foreign_name =
+            match payload with
+            | PStr
+                [ { pstr_desc =
+                      Pstr_eval
+                        ( { pexp_desc = Pexp_ident { txt = Lident lident; _ }
+                          ; _
+                          }
+                        , _ )
+                  ; _
+                  }
+                ] -> lident
+            | _ -> Location.raise_errorf ~loc "Unsupported payload for @foreign"
+          in
+          let foreign_query =
+            mk_get_by_id
+              ~loc
+              ~selections
+              ~table_name
+              ~key:foreign_key
+              ~foreign_name
+              ~foreign_key
+          in
+          let body = [%expr [%rapper get_many [%e foreign_query] record_out]] in
+          (* let id_argument = ppat_var ~loc { txt = foreign_key; loc } in *)
+          (* let expr = pexp_fun ~loc Nolabel None id_argument body in *)
+          let pat = ppat_var ~loc { txt = "get_by_" ^ foreign_key; loc } in
+          let binding =
+            Ast_builder.Default.value_binding ~loc ~pat ~expr:body
+          in
+          pstr_value ~loc Nonrecursive [ binding ])
       in
-      let create_values =
-        List.map non_id_labels ~f:label_to_input |> String.concat ~sep:","
+      let foreign =
+        match foreign with
+        | Some foreign -> foreign
+        | None -> [%stri let _ = 0]
       in
-      let create =
-        "INSERT INTO "
-        ^ table_name
-        ^ " ("
-        ^ create_select
-        ^ ") VALUES ("
-        ^ create_values
-        ^ ") RETURNING "
-        ^ label_to_output id_label
-      in
-      let create = make_str_const create loc in
-      let mirror_type_name = "data" in
-      let mirror_type =
-        ptyp_constr
-          ~loc
-          (Located.lident ~loc mirror_type_name)
-          (List.map rec_type.ptype_params ~f:snd)
-      in
-      let mirror_type_decl =
+      let select = mk_select ~selections ~table_name ~id_label ~loc in
+      let select_many = mk_select_many ~selections ~table_name ~loc in
+      let create = mk_create ~id_label ~non_id_labels ~table_name ~loc in
+      let data_type =
         type_declaration
-          ~name:(Located.mk ~loc mirror_type_name)
-          ~params:rec_type.ptype_params
+          ~loc
+          ~name:Location.{ txt = "data"; loc }
+          ~params:[]
           ~cstrs:[]
-          ~kind:rec_type.ptype_kind
-          ~private_:rec_type.ptype_private
-          ~manifest:(Some mirror_type)
-          ()
+          ~kind:(Ptype_record non_id_labels)
+          ~manifest:None
+          ~private_:Public
       in
-      (* [ pstr_type ~loc Recursive [ mirror_type_decl ] ] in *)
+      let data_type = pstr_type ~loc Nonrecursive [ data_type ] in
       [%stri
         include struct
-          (* type data = *)
-          (*   { name : string *)
-          (*   ; user_id : int *)
-          (*   } *)
-          (* type data [%type { name : string; user_id; int }] *)
-          [%e mirror_type_decl]
+          [%%i data_type]
 
           let create = [%rapper get_one [%e create]]
           let create_record = [%rapper get_one [%e create] record_in]
-          let read = [%rapper get_opt [%e selection] record_out]
+          let read = [%rapper get_opt [%e select] record_out]
+          let read_all = [%rapper get_many [%e select_many] record_out]
+
+          [%%i foreign]
         end]
     | _ -> Location.raise_errorf ~loc "Cannot derive anything for this type")
 ;;
